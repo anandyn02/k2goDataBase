7bc04e59486b91e3e0acf15634550e1a7a88c038
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-11-09 12:38:50 +0530

    Update K2GoCourseContent.swift

--- Sources/K2GoDataBase/K2GoCourseContent.swift
+++ Sources/K2GoDataBase/K2GoCourseContent.swift
@@ -14,9 +14,12 @@ public class K2GoCourseContent: Object, Decodable {
     @objc dynamic public var id: Int = 0
     @objc dynamic public var visible: Int = 0
     @objc dynamic public var summary: String = ""
+    @objc dynamic public var isFavorite: Bool = false
 
     public var modules = List<K2GoCourseModule>()
-
+    
+    public var sessions = List<String>()
+    public var content = List<K2GoTopic>()
     public override static func primaryKey() -> String? { return "id" }
 
 }
@@ -28,4 +31,11 @@ public class K2GoCourseModule: Object, Decodable {
     @Persisted public var name: String = ""
     @Persisted public var visible: Int = 0
     @Persisted public var contextid: Int = 0
+}
+
+public class  K2GoTopic: Object, Decodable  {
+    
+    @Persisted public var header: String = ""
+    @Persisted public var content: String = ""
+    @Persisted public var isSelected: Bool = false
 }
