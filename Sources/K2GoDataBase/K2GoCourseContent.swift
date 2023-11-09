248fc218f4c94374059455f6d374253aa083f450
anandyn2002 <45676373+anandyn2002@users.noreply.github.com>
2023-11-09 12:11:16 +0530

    Create K2GoCourseContent.swift

---
+++ Sources/K2GoDataBase/K2GoCourseContent.swift
@@ -0,0 +1,32 @@
+//
+//  K2GoCourseContent.swift
+//  
+//
+//  Created by Anand  on 09/11/23.
+//
+
+import UIKit
+import RealmSwift
+
+public class K2GoCourseContent: Object, Decodable {
+    
+    @objc dynamic public var name: String = ""
+    @objc dynamic public var id: Int = 0
+    @objc dynamic public var visible: Int = 0
+    @objc dynamic public var summary: Int = 0
+    @objc dynamic public var content: String = ""
+
+    public var modules = List<K2GoCourseModule>()
+
+    public override static func primaryKey() -> String? { return "id" }
+
+}
+
+public class K2GoCourseModule: Object, Decodable {
+    
+    @Persisted public var id: Int = 0
+    @Persisted public var url: String = ""
+    @Persisted public var name: String = ""
+    @Persisted public var visible: Int = 0
+    @Persisted public var contextid: Int = 0
+}
